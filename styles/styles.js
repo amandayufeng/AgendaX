7ab1414dcf3118ef641c1dac7ada384606f5b336
Amanda Xia <amandayufeng@gmail.com>
2023-05-25 09:33:13 -0400

    Create Stylesheet

---
+++ styles/styles.js
@@ -0,0 +1,52 @@
+import { StyleSheet,Dimensions } from 'react-native';
+import { color } from 'react-native-reanimated';
+
+//to be changed but please use these and add as needed
+export const STYLES = StyleSheet.create({
+  background: {
+    backgroundColor:'white',   
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+  },
+  flex: {
+    flex: 1
+  },
+  centerContainer: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 30,
+    color: 'white'
+  },
+  heading: {
+    fontSize: 25
+  },
+  selectButtonContainer: {
+    margin: 20,
+    borderRadius: 5
+  },
+  selectButtonTitle: {
+    padding: 10,
+    fontSize: 18
+  },
+  // ADD BELOW
+  imageContainer: {
+    marginVertical: 20,
+    borderWidth: 5,
+    borderColor: 'black'
+  },
+  imageBox: {
+    width: 256,
+    height: 256
+  }
+});
+
+export const COLORS = {
+  primaryDark: '#22212c',
+  primaryLight: '#f8f8f2',
+  primaryRed: '#ff5555',
+  primaryPink: '#ff80bf',
+  primaryYellow: '#ffff80',
+  primaryOrange: '#ff9580'
+};
